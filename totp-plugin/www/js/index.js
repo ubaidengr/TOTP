c366b2d75ab9f9d9e10a68d0774198528b9bf43d
Murtuza Saifee <>
2020-04-14 19:21:21 +0400

    adding ios support

--- totp-plugin/www/js/index.js
+++ totp-plugin/www/js/index.js
@@ -39,7 +39,7 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('Received Events: ' + id);
     }
 };
 
